00501425f06ef675944475577cca51c1208f21bc
apostolescus <54913375+apostolescus@users.noreply.github.com>
2019-12-02 17:37:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,44 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#define NUM 128
+
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+
+int main() {
+
+	FILE* f; 
+
+	int buffer[256];
+	unsigned short int c = 0;
+	double H = 0;
+
+	memset(buffer, 0, NUM*4);
+	
+	f = fopen("intro1.exe", "rb");
+
+	if (f == NULL) {
+		perror("Error: ");
+		return -1;
+	}
+	//find the size of the file
+	fseek(f, 0, SEEK_END);
+	int fileLen = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	//get the character frequency
+	for (int i = 0;i < fileLen;i++) {
+		c = fgetc(f);
+		buffer[c]++;
+	}
+	//calculate  Shannon Entropy 
+	for (int i = 0;i < NUM;i++)
+		H = H- (double)buffer[i] / fileLen * log2((double)buffer[i] / fileLen);
+
+	printf("entropy is : %lf\n", H);
+
+	return 0;
+
+}
